8fb948dce1ef872df29c418d57147e40c261cd04
yujin65 <hyj23510@naver.com>
2023-09-07 15:39:50 +0900

    P

--- Project1/a.cpp
+++ Project1/a.cpp
@@ -2,7 +2,22 @@
 
 int main()
 {
-	printf("Hello world!\n");
-	printf("Hello SW!\n");
+	int a;
+
+	scanf_s("%d", &a);
+	printf("%d층 피라미드\n", a);
+
+	for (int i=1;i<=a;i++)
+	{
+		for (int j=1;j<=a-i;j++)
+		{
+			printf(" ");
+		}
+		for (int j = 1; j <= 2 * i - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
