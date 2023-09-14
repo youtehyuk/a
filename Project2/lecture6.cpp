0b221592768b9964f517d0b89ab9d760e74dfa19
limjoonhyuk <144293477+koyoungsan@users.noreply.github.com>
2023-09-14 16:02:37 +0900

    학점 판별

--- Project2/lecture6.cpp
+++ Project2/lecture6.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
 int main()
 {
+	int p;
+	printf("점수 입력 : ");
+	scanf_s("%d", &p);
 
-	int a;
-	printf("정수 입력 :");
-	scanf_s("%d", &a);
-	if (a%2==0)
-		printf("%d(은)는 짝수이다.", a);
-	else
-		printf("%d(은)는 홀수이다.", a);
+	if (p >= 90) printf("A 학점 입니다.");
+	else if (p >= 80) printf("B 학점 입니다.");
+	else if (p >= 70) printf("C 학점 입니다.");
+	else if (p >= 60) printf("D 학점 입니다.");
+	else if (p < 60) printf("F 학점 입니다.");
+	else if (p < 0 || p>100)
+		printf("잘못 입력하였습니다.");
+
 	return 0;
 }
