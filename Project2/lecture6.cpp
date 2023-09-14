64de20cfecf00b31de969a1c2f839244b6849624
limjoonhyuk <144293477+koyoungsan@users.noreply.github.com>
2023-09-14 15:34:27 +0900

    짝수 홀수

--- Project2/lecture6.cpp
+++ Project2/lecture6.cpp
@@ -5,11 +5,9 @@ int main()
 	int a;
 	printf("정수 입력 :");
 	scanf_s("%d", &a);
-	if (a > 0)
-		printf("%d(은)는 양수이다.", a);
-	else if (a == 0)
-		printf("%d(은)는 0이다.", a);
+	if (a%2==0)
+		printf("%d(은)는 짝수이다.", a);
 	else
-		printf("%d(은)는 음수이다.", a);
+		printf("%d(은)는 홀수이다.", a);
 	return 0;
 }
