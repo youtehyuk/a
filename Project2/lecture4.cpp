94c3e0136cdfff65d064dd1a0e62b5f1848f6790
limjoonhyuk <144293477+koyoungsan@users.noreply.github.com>
2023-09-14 16:01:24 +0900

    윤년 판별

--- Project2/lecture4.cpp
+++ Project2/lecture4.cpp
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-	int a, b, c, val1, val2, val3, val4, val5;
-	a = 0, b = 5, c = 8;
+	int a;
 
-	val1 = a && b;
-	val2 = a || c;
-	val3 = (a > b) && (a < c);
-	val4 = !val3;
-	val5 = b && c;
-	printf("a= 0, b= 5, c= 8일 때\n");
-	printf("1. a&&b : %d\n", val1);
-	printf("2. a||c : %d\n", val2);
-	printf("3. (a > b) && (a < c) = %d\n", val3);
-	printf("4. !val3 : %d\n", val4);
-	printf("1. b && c : %d\n", val5);
+	printf("년도 입력:");
+	scanf_s("%d", &a);
+
+	if (((a % 4 == 0)&&(a % 100 != 0))||(a%400==0))
+	{	
+		printf("윤년 입니다.");
+	}
+	else
+		printf("윤년이 아닙니다.");
 }
